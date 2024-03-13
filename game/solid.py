33a250f5b0f877e86b7cb9d684a40c36e04e6a9a
Volqano <theriverpl@gmail.com>
2024-03-13 10:40:38 +0100

    Fixed bug with sand randomness

--- game/solid.py
+++ game/solid.py
@@ -13,11 +13,15 @@ class MovableSolid(Solid):
 
     def move(self, sr):
         if self.y + 1 < sr.size:
-            direction = random.choice([-1, 1])
+            choice = [((self.x + 1 < sr.size and not sr.surface[self.y + 1][self.x + 1]), 1),
+                      ((not sr.surface[self.y + 1][self.x - 1]), -1)]
+            random.shuffle(choice)
+            condition1, direction1 = choice[0]
+            condition2, direction2 = choice[1]
             if not sr.surface[self.y + 1][self.x]:
                 self.change_position(sr, self.x, self.y, self.x, self.y + 1)
-            elif self.x + 1 < sr.size and not sr.surface[self.y + 1][self.x + direction]:
-                self.change_position(sr, self.x, self.y, self.x + direction, self.y + 1)
-            elif self.x + 1 < sr.size and not sr.surface[self.y + 1][self.x - direction]:
-                self.change_position(sr, self.x, self.y, self.x - direction, self.y + 1)
+            elif condition1:
+                self.change_position(sr, self.x, self.y, self.x + direction1, self.y + 1)
+            elif condition2:
+                self.change_position(sr, self.x, self.y, self.x + direction2, self.y + 1)
 
