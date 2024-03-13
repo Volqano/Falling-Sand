16642cacc97f701bf8591d2449380a18338a31b0
Volqano <theriverpl@gmail.com>
2024-03-13 10:42:40 +0100

    Added liquid and gas classes

--- game/gas.py
+++ game/gas.py
@@ -1,1 +1,6 @@
+from game.element import *
 
+
+class Gas(Element):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
--- game/liquid.py
+++ game/liquid.py
@@ -1,1 +1,6 @@
+from game.element import *
 
+
+class Liquid(Element):
+    def __init__(self, x, y, color):
+        super().__init__(x, y, color)
